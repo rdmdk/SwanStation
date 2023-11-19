999d3da31d5716118a5f65fab2d72eb91ed4360e
Redmond <rdmdk@users.noreply.github.com>
2023-11-19 08:38:50 +0000

    Update js.js

--- js.js
+++ js.js
@@ -1,34 +1,35 @@
-let m = document.getElementById("m");
-let s = document.getElementById("s");
-let mm = localStorage.minute ? localStorage.minute : 108;
-let ss = localStorage.second ? localStorage.second : 00;
-var silenced = false;
+const main = document.queryselector('main');
+const m = document.getElementById("m");
+const s = document.getElementById("s");
+let mm = localStorage.minute ? Number(localStorage.minute) : 108;
+let ss = localStorage.second ? Number(localStorage.second) : 00;
+let silenced = false;
 
 function minutes() {
-	let a = mm.toString().split("");
-	let b = mm < 100 && mm >= 10 ? "<span>0</span>" : mm < 10 && mm >= 0 ? "<span>0</span><span>0</span>" : "";
+	let a = mm.toString().split('');
+	let b = mm < 100 && mm >= 10 ? '<span>0</span>' : mm < 10 && mm >= 0 ? '<span>0</span><span>0</span>' : '';
 	m.innerHTML = b;
-	a.forEach((c) => m.insertAdjacentHTML("beforeend", "<span>" + c + "</span>"));
+	a.forEach(c => m.insertAdjacentHTML('beforeend', '<span>' + c + '</span>'));
 }
 
 function seconds() {
-	let a = ss.toString().split("");
-	let b = ss < 10 ? "<span>0</span>" : "";
+	let a = ss.toString().split('');
+	let b = ss < 10 ? '<span>0</span>' : '';
 	s.innerHTML = b;
-	a.forEach((c) => s.insertAdjacentHTML("beforeend", "<span>" + c + "</span>"));
+	a.forEach(c => s.insertAdjacentHTML('beforeend', '<span>' + c + '</span>'));
 }
 
 function sounds() {
-	const alarm = new Audio("alarm.mp3");
-	const beep = new Audio("beep.mp3");
-	const crash = new Audio("crash.mp3");
-	const tick = new Audio("tick.mp3");
+	const alarm = new Audio('alarm.mp3');
+	const beep = new Audio('beep.mp3');
+	const crash = new Audio('crash.mp3');
+	const tick = new Audio('tick.mp3');
 	alarm.volume = 0.5;
 	beep.volume = 0.5;
 	crash.volume = 1;
 	tick.volume = 0.25;
 	
-	if (window.location.hash === "#debug") {
+	if (window.location.search === '?debug') {
 	
 		let i = setInterval(() => {
 			if (document.hidden || silenced) {}
@@ -57,7 +58,7 @@ function sounds() {
 			}
 		}, 1e3);
 	
-	} else if (window.location.hash === "#debug2") {
+	} else if (window.location.search === '?debug2') {
 		
 		// Tick
 		let i = setInterval(() => {
@@ -199,66 +200,68 @@ function sounds() {
 }
 function initiation() {
 	if (!localStorage.initiation) {
-		if (document.querySelector("iframe")) {
-			let a = document.querySelector("iframe");
+		if (document.querySelector('iframe')) {
+			let a = document.querySelector('iframe');
 			a.parentNode.removeChild(a);
 		}
-		document.querySelector(".screen").insertAdjacentHTML("afterbegin", "<div id='x'></div>");
-		new YT.Player("x", {
-			videoId: "UNFDHgjrlK8",
+		document.querySelector('.screen').insertAdjacentHTML('afterbegin', '<div id="x"></div>');
+		new YT.Player('x', {
+			videoId: 'UNFDHgjrlK8',
 			playerVars: {
 				autoplay: 1,
 				modestbranding: 1,
 				rel: 0,
 				playsinline: 1,
 				iv_load_policy: 3,
-				origin: "https://www.youtube.com"
+				origin: 'https://www.youtube.com'
 			},
 			events: {
-				"onStateChange": onPlayerStateChange
+				'onStateChange': onPlayerStateChange
 			}
 		});
 	}
 	function onPlayerStateChange(event) {
 		if (event.data === 0) {
-			let a = document.querySelector("iframe");
+			let a = document.querySelector('iframe');
 			a.parentNode.removeChild(a);
-			localStorage.initiation = "initiated";
+			localStorage.initiation = 'initiated';
 		}
        }
 }
 
 function theend() {
-	setTimeout(() => { m.innerHTML = "<span class='black'>𓋿</span>" }, 3e3);
-	setTimeout(() => { m.innerHTML += "<span class='black'>𓏲</span>" }, 4e3);
-	setTimeout(() => { m.innerHTML += "<span class='black'>𓍑</span>" }, 5e3);
-	setTimeout(() => { s.innerHTML = "<span class='red'>𓅂</span>" }, 6e3);
-	setTimeout(() => { s.innerHTML += "<span class='red'>𓏱</span>" }, 7e3);
-	setTimeout(() => document.querySelector("main").classList.add("danger"), 10e3);
+	setTimeout(() => m.innerHTML = '<span class="black">𓋿</span>', 3e3);
+	setTimeout(() => m.insertAdjacentHTML('beforeend', '<span class="black">𓏲</span>'), 4e3);
+	setTimeout(() => m.insertAdjacentHTML('beforeend', '<span class="black">𓍑</span>'), 5e3);
+	setTimeout(() => s.innerHTML = '<span class="red">𓅂</span>', 6e3);
+	setTimeout(() => s.insertAdjacentHTML('beforeend', '<span class="red">𓏱</span>'), 7e3);
+	setTimeout(() => main.classList.add('danger'), 10e3);
 	setTimeout(() => {
-		setInterval(() => document.getElementById("input").value += "System Failure ", 500);
+		const inp = document.getElementById('input');
+		setInterval(() => inp.value += 'System Failure ', 500);
 	}, 14500);
 	setTimeout(() => {
-		document.querySelector("main").style.opacity = 0;
+		const bg = document.querySelector('.bg');
+		main.style.opacity = 0;
 		setInterval(() => {
-			var r1 = Math.floor(Math.random() - 0.5) * 3;
-			var r2 = Math.floor(Math.random() - 0.5) * 5;
-			document.querySelector(".bg").style.marginTop = r1 + "em";
-			document.querySelector(".bg").style.marginLeft = r2 + "em";
+			const r1 = Math.floor(Math.random() - 0.5) * 3;
+			const r2 = Math.floor(Math.random() - 0.5) * 5;
+			bg.style.marginTop = r1 + 'em';
+			bg.style.marginLeft = r2 + 'em';
 		}, 150);
 	}, 19e3);
 	setTimeout(() => {
-		localStorage.removeItem("minute");
-		localStorage.removeItem("second");
-		localStorage.removeItem("initiation");
+		localStorage.removeItem('minute');
+		localStorage.removeItem('second');
+		localStorage.removeItem('initiation');
 		window.location.reload();
 	}, 31e3);
 }
 
 function update() {
-	ss--;
+	ss -= 1;
 	if (ss < 0) {
-		mm--;
+		m -= 1;
 		ss = 59;
 		minutes();
 	}
@@ -273,63 +276,61 @@ function update() {
 };
 
 function execute() {
-	let a = document.getElementById("input");
-	if (document.querySelector("main").classList.contains("danger")) return false;
-	else if (a.value === "4 8 15 16 23 42") {
+	let a = document.getElementById('input');
+	if (main.classList.contains('danger')) return false;
+	else if (a.value === '4 8 15 16 23 42') {
 		if (mm < 4) {
 			mm = 108;
 			ss = 00;
-			a.value = "";
+			a.value = '';
 			a.focus();
 		} else {
-			a.value = "Not yet";
+			a.value = 'Not yet';
 			setTimeout(() => {
-				a.value = "";
+				a.value = '';
 				a.focus();
 			}, 4e3);
 		}
 	} else {
-		a.value = "";
+		a.value = '';
 		a.focus();
 	}
 };
 
 function silence() {
-	if (silenced) {
-		document.getElementById("silence").className = "silence";
-		silenced = false;
-	} else {
-		document.getElementById("silence").className = "silenced";
-		silenced = true;
-	}
+	const s = document.getElementById('silence');
+	silenced = !silenced;
+	s.className = silenced ? 'silenced' : 'silence';
 }
-document.getElementById("go").addEventListener("click", (e) => {
+
+const go = document.getElementById('go');
+go.addEventListener('click', e => {
 	sounds();
 	setTimeout(() => {
 		minutes();
 		seconds();
 		si = setInterval(() => update(), 1e3);
-		document.getElementById("input").focus();
-		document.body.removeChild(document.getElementById("go"));
+		document.getElementById('input').focus();
+		go.remove();
 	}, 3e3);
 	setTimeout(() => initiation(), 7e3);
 });
-document.getElementById("execute").addEventListener("click", () => execute());
-document.querySelector(".monitor").addEventListener("click", () => document.getElementById("input").focus());
-document.getElementById("silence").addEventListener("click", () => silence());
-document.querySelector("i").addEventListener("click", (e) => {
-	if (document.querySelector("main").classList.contains("danger")) return false;
+document.getElementById('execute').addEventListener('click', () => execute());
+document.querySelector('.monitor').addEventListener('click', () => document.getElementById('input').focus());
+document.getElementById('silence').addEventListener('click', () => silence());
+document.querySelector('i').addEventListener('click', e => {
+	if (main.classList.contains('danger')) return false;
 	else if (e.shiftKey) {
 		localStorage.clear();
 		mm = 1;
 		ss = 30;
-		localStorage.initiation = "initiated";
+		localStorage.initiation = 'initiated';
 		setTimeout(() => window.location.reload(), 1e3);
 	} else {
-		localStorage.removeItem("initiation");
+		localStorage.removeItem('initiation');
 		initiation();
 	}
 });
 setInterval(() => {
-	if (!document.querySelector(".input:focus") && !document.querySelector("main").classList.contains("danger")) document.querySelector(".input").focus();
+	if (!document.querySelector('.input:focus') && !main.classList.contains('danger')) document.querySelector('.input').focus();
 }, 100);
